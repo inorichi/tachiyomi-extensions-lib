2530c8ce3a67405ca7e3c27ccf0575d9220fcafc
Carlos Escobedo <cargo8005@gmail.com>
2017-12-15 13:40:16 -0500

    added preferencesHelper

---
+++ library/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
@@ -0,0 +1,11 @@
+package eu.kanade.tachiyomi.data.preference
+
+import eu.kanade.tachiyomi.source.Source
+
+
+class PreferencesHelper {
+
+    fun sourceUsername(source: Source): String = throw Exception("Stub!")
+
+    fun sourcePassword(source: Source): String = throw Exception("Stub!")
+}
