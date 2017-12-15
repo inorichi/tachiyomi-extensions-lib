b2a70536b6d824fde03993f5f9a1d2407a2d94f2
Carlos Escobedo <cargo8005@gmail.com>
2017-12-15 15:20:18 -0500

    adjusted

--- library/src/main/java/eu/kanade/tachiyomi/network/PersistentCookieStore.kt
+++ library/src/main/java/eu/kanade/tachiyomi/network/PersistentCookieStore.kt
@@ -1,7 +1,13 @@
 package eu.kanade.tachiyomi.network
 
 import android.content.Context
+import okhttp3.HttpUrl
+import java.net.URI
 
 class PersistentCookieStore(context: Context) {
-        fun get() : PersistentCookieStore = throw Exception("Stub!")
+
+        fun get(url: HttpUrl) : PersistentCookieStore = throw Exception("Stub!")
+
+        fun get(uri: URI) : PersistentCookieStore = throw Exception("Stub!")
+
 }
